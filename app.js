edf3fd30e766222715864ea17e3e475f7f2b546e
binil benny <85955570+binilbenny@users.noreply.github.com>
2022-06-09 21:19:47 -0400

    create element object

---
+++ app.js
@@ -0,0 +1,31 @@
+const oneButton = document.getElementById('1');
+const twoButton = document.getElementById('2');
+const threeButton = document.getElementById('3');
+const fourButton = document.getElementById('4');
+const fiveButton = document.getElementById('5');
+const sixButton = document.getElementById('6');
+const sevenButton = document.getElementById('7');
+const eightButton = document.getElementById('8');
+const nineButton = document.getElementById('9');
+const zeroButton = document.getElementById('0');
+
+const multibutton = document.getElementById('multi')
+const dividButton = document.getElementById('divid');
+const dotButton = document.getElementById('dot');
+const clearButton = document.getElementById('clear');
+const positveNagativeButton = document.getElementById('positveNagative');
+const squreRootButton = document.getElementById('squreRoot');
+const  minusButton = document.getElementById('minus');
+const percentageButton = document.getElementById('percentage');
+const plusButton = document.getElementById('plus');
+const equalButton = document.getElementById('equal');
+const backspaceButton = document.getElementById('backspace');
+
+const primaryDisplay = document.querySelector('.primary');
+const secondaryDisplay = document.querySelector('.secondary');
+
+
+
+
+
+
